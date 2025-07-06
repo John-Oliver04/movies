a6c9b361d8d0b3950b62b7708b8d50c17f517f5a
John Oliver G Virola <johnolivervirola4@gmail.com>
2025-07-06 15:56:54 +0800

    committing changes in WEb Config

---
+++ src/main/java/dev/virola/movies/config/WebConfig.java
@@ -0,0 +1,24 @@
+package dev.virola.movies.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Change for production
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
